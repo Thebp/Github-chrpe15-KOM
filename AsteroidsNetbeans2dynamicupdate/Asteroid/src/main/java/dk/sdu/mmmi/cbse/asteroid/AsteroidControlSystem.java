3a57f150db81460edbe2c3d727ab2a9fab4bd6aa
Thebp <chrpe15@student.sdu.dk>
2017-03-20 13:56:31 +0100

    Dynamic update added

---
+++ AsteroidsNetbeans2dynamicupdate/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidControlSystem.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.asteroid;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.EntityType;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.events.Event;
+import static dk.sdu.mmmi.cbse.common.events.EventType.ASTEROID_SPLIT;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.Random;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Christian
+ */
+@ServiceProvider(service = IEntityProcessingService.class)
+public class AsteroidControlSystem implements IEntityProcessingService, IGamePluginService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity asteroid : world.getEntities(EntityType.ASTEROIDS)) {
+            float x = asteroid.getX();
+            float y = asteroid.getY();
+            float dx = asteroid.getDx();
+            float dy = asteroid.getDy();
+            float radians = asteroid.getRadians();
+            int rotationSpeed = asteroid.getRotationSpeed();
+            float dt = gameData.getDelta();
+            float acceleration = asteroid.getAcceleration();
+            float deceleration = asteroid.getDeacceleration();
+            float maxSpeed = asteroid.getMaxSpeed();
+            float[] shapeX = asteroid.getShapeX();
+            float[] shapeY = asteroid.getShapeY();
+            boolean up = true;
+            Random rand = new Random();
+
+            if (rand.nextBoolean() == true) {
+                radians += rotationSpeed * dt;
+            } else if (rand.nextBoolean() == false) {
+                radians -= rotationSpeed * dt;
+            }
+
+            if (asteroid.getIsHit() == true) {
+                gameData.addEvent(new Event(ASTEROID_SPLIT, asteroid.getID()));
+            }
+
+            // accelerating
+            if (up == true) {
+                dx += Math.cos(radians) * acceleration * dt;
+                dy += Math.sin(radians) * acceleration * dt;
+            }
+
+            // deceleration
+            float vec = (float) Math.sqrt(dx * dx + dy * dy);
+            if (vec > 0) {
+                dx -= (dx / vec) * deceleration * dt;
+                dy -= (dy / vec) * deceleration * dt;
+            }
+            if (vec > maxSpeed) {
+                dx = (dx / vec) * maxSpeed;
+                dy = (dy / vec) * maxSpeed;
+            }
+
+            // set position
+            x += dx * dt;
+            y += dy * dt;
+
+            shapeX[0] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 1) * asteroid.getRadius());
+            shapeY[0] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 1) * asteroid.getRadius());
+
+            shapeX[1] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 1.25) * asteroid.getRadius());
+            shapeY[1] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 1.25) * asteroid.getRadius());
+
+            shapeX[2] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 1.5) * asteroid.getRadius());
+            shapeY[2] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 1.5) * asteroid.getRadius());
+
+            shapeX[3] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 1.75) * asteroid.getRadius());
+            shapeY[3] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 1.75) * asteroid.getRadius());
+
+            shapeX[4] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 2) * asteroid.getRadius());
+            shapeY[4] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 2) * asteroid.getRadius());
+
+            shapeX[5] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 2.25) * asteroid.getRadius());
+            shapeY[5] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 2.25) * asteroid.getRadius());
+
+            shapeX[6] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 2.5) * asteroid.getRadius());
+            shapeY[6] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 2.5) * asteroid.getRadius());
+
+            shapeX[7] = (float) (asteroid.getX() + Math.cos(radians + Math.PI * 2.75) * asteroid.getRadius());
+            shapeY[7] = (float) (asteroid.getY() + Math.sin(radians + Math.PI * 2.75) * asteroid.getRadius());
+
+            asteroid.setX(x);
+            asteroid.setY(y);
+            asteroid.setDx(dx);
+            asteroid.setDy(dy);
+            asteroid.setRadians(radians);
+            asteroid.setRotationSpeed(rotationSpeed);
+            asteroid.setAcceleration(acceleration);
+            asteroid.setDeacceleration(deceleration);
+            asteroid.setMaxSpeed(maxSpeed);
+            asteroid.setShapeX(shapeX);
+            asteroid.setShapeY(shapeY);
+
+            for (Event event : gameData.getEvents()) {
+                if (event.getType() == ASTEROID_SPLIT) {
+                    Entity asteroids = new Entity();
+                    asteroids.setType(EntityType.ASTEROIDS);
+                    if(event.getEntityID().equals(asteroid.getID())){
+                         asteroids.setPosition(asteroid.getX() + 10 , asteroid.getY() + 10);
+                    }
+                    
+                   
+
+                    asteroids.setMaxSpeed(150);
+                    asteroids.setAcceleration(75);
+                    asteroids.setDeacceleration(5);
+                    if(asteroids.getRadius() == 32){
+                        asteroids.setRadius(16);
+                    }else if(asteroids.getRadius() == 16){
+                        asteroids.setRadius(8);
+                    }
+                    asteroids.setShapeX(new float[8]);
+                    asteroids.setShapeY(new float[8]);
+                    asteroids.setRadians(3.1415f / 2);
+                    asteroids.setRotationSpeed(3);
+                    world.addEntity(asteroids);
+                    world.removeEntity(asteroid);
+                    
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ AsteroidsNetbeans2dynamicupdate/Bullet/src/main/java/dk/sdu/mmmi/cbse/bullet/BulletControlSystem.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.bullet;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.EntityType;
+import static dk.sdu.mmmi.cbse.common.data.EntityType.BULLET;
+import static dk.sdu.mmmi.cbse.common.data.EntityType.ENEMY;
+import static dk.sdu.mmmi.cbse.common.data.EntityType.PLAYER;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.events.Event;
+import static dk.sdu.mmmi.cbse.common.events.EventType.ENEMY_SHOOT;
+import static dk.sdu.mmmi.cbse.common.events.EventType.PLAYER_SHOOT;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Christian
+ */
+@ServiceProvider(service = IEntityProcessingService.class)
+public class BulletControlSystem implements IEntityProcessingService, IGamePluginService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        if (!gameData.getEvents().isEmpty()) {
+            for (Event event : gameData.getEvents()) {
+                if (event.getType() == PLAYER_SHOOT){
+                    for (Entity entity : world.getEntities(PLAYER)) {
+                        if (entity.getID().equals(event.getEntityID())) {
+                            Entity bullet = new Entity();
+                            bullet.setType(BULLET);
+                            bullet.setRadius(4);
+                            bullet.setX(entity.getX() + (float) Math.cos(entity.getRadians()) * (entity.getRadius() + bullet.getRadius() + 1));
+                            bullet.setY(entity.getY() + (float) Math.sin(entity.getRadians()) * (entity.getRadius() + bullet.getRadius() + 1));
+                            bullet.setDx(entity.getDx());
+                            bullet.setDy(entity.getDy());
+                            bullet.setAcceleration(10000);
+                            bullet.setDeacceleration(0);
+                            bullet.setMaxSpeed(400);
+                            bullet.setExpiration(2.5f);
+                            bullet.setRadians(entity.getRadians());
+                            world.addEntity(bullet);
+                            gameData.removeEvent(event);
+                        }
+                    }
+                }else if(event.getType() == ENEMY_SHOOT){
+                    for(Entity enemy : world.getEntities(ENEMY)){
+                        if(enemy.getID().equals(event.getEntityID())){
+                            Entity bullet = new Entity();
+                            bullet.setType(BULLET);
+                            bullet.setRadius(4);
+                            bullet.setX(enemy.getX() + (float) Math.cos(enemy.getRadians()) * (enemy.getRadius() + bullet.getRadius() + 1));
+                            bullet.setY(enemy.getY() + (float) Math.sin(enemy.getRadians()) * (enemy.getRadius() + bullet.getRadius() + 1));
+                            bullet.setDx(enemy.getDx());
+                            bullet.setDy(enemy.getDy());
+                            bullet.setAcceleration(10000);
+                            bullet.setDeacceleration(0);
+                            bullet.setMaxSpeed(400);
+                            bullet.setExpiration(2.5f);
+                            bullet.setRadians(enemy.getRadians());
+                            world.addEntity(bullet);
+                            gameData.removeEvent(event);
+                        }
+                    }
+                }
+            }
+        }
+        for (Entity bullet : world.getEntities(EntityType.BULLET)) {
+            if(bullet.getExpiration() < 0){
+                world.removeEntity(bullet);
+            }
+            if(bullet.getIsHit() == true){
+                world.removeEntity(bullet);
+                bullet.setIsHit(false);
+            }
+            float x = bullet.getX();
+            float y = bullet.getY();
+            float dx = bullet.getDx();
+            float dy = bullet.getDy();
+            float radians = bullet.getRadians();
+            //int rotationSpeed = bullet.getRotationSpeed();
+            float dt = gameData.getDelta();
+            float acceleration = bullet.getAcceleration();
+            float deceleration = bullet.getDeacceleration();
+            float maxSpeed = bullet.getMaxSpeed();
+            float[] shapeX = bullet.getShapeX();
+            float[] shapeY = bullet.getShapeY();
+            //boolean up = true;
+
+            // accelerating
+            
+                dx += Math.cos(radians) * acceleration * dt;
+                dy += Math.sin(radians) * acceleration * dt;
+            
+
+            // deceleration
+            float vec = (float) Math.sqrt(dx * dx + dy * dy);
+            if (vec > 0) {
+                dx -= (dx / vec) * deceleration * dt;
+                dy -= (dy / vec) * deceleration * dt;
+            }
+            if (vec > maxSpeed) {
+                dx = (dx / vec) * maxSpeed;
+                dy = (dy / vec) * maxSpeed;
+            }
+
+            // set position
+            x += dx * dt;
+            y += dy * dt;
+
+            shapeX[0] = (float) (bullet.getX() + Math.cos(radians + Math.PI * 1) * bullet.getRadius());
+            shapeY[0] = (float) (bullet.getY() + Math.sin(radians + Math.PI * 1) * bullet.getRadius());
+
+            shapeX[1] = (float) (bullet.getX() + Math.cos(radians + Math.PI * 2) * bullet.getRadius());
+            shapeY[1] = (float) (bullet.getY() + Math.sin(radians + Math.PI * 2) * bullet.getRadius());
+
+            shapeX[2] = (float) (bullet.getX() + Math.cos(radians + Math.PI * 3) * bullet.getRadius());
+            shapeY[2] = (float) (bullet.getY() + Math.sin(radians + Math.PI * 3) * bullet.getRadius());
+
+            shapeX[3] = (float) (bullet.getX() + Math.cos(radians + Math.PI * 4) * bullet.getRadius());
+            shapeY[3] = (float) (bullet.getY() + Math.sin(radians + Math.PI * 4) * bullet.getRadius());
+
+            bullet.setX(x);
+            bullet.setY(y);
+            bullet.setDx(dx);
+            bullet.setDy(dy);
+            bullet.setRadians(radians);
+            //asteroid.setRotationSpeed(rotationSpeed);
+            bullet.setAcceleration(acceleration);
+            bullet.setDeacceleration(deceleration);
+            bullet.setMaxSpeed(maxSpeed);
+            bullet.setShapeX(shapeX);
+            bullet.setShapeY(shapeY);
+            bullet.setExpiration(bullet.getExpiration() - dt);
+        }
+    }
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
